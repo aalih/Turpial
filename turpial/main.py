11edf4b5b35e465476e487a5a7ab1cd2343d29ee
Alfonso Ali <alfonso.ali@gmail.com>
2010-08-05 17:39:09 -0400

    Usar proxy del sistema sin necesidad de iniciarlo desde la consola
    Arregla el issue 70 (google code) y habilita la actualizacion de la
    configuracion del proxy si es cambiado mientras se ejecuta el programa.

--- turpial/main.py
+++ turpial/main.py
@@ -12,6 +12,9 @@ import sys
 import base64
 import logging
 from optparse import OptionParser
+import urllib
+import urllib2
+import gconf
 
 from turpial.ui.gtk.main import Main as _GTK
 from turpial.api.servicesapi import HTTPServices
@@ -84,6 +87,13 @@ class Turpial:
         
         if self.testmode:
             self.log.debug('Modo Pruebas Activado')
+
+        gclient = gconf.client_get_default()
+        self.__set_proxy(gclient)
+        gclient.add_dir('/system/http_proxy', gconf.CLIENT_PRELOAD_NONE)
+        gclient.notify_add('/system/http_proxy', self.__set_proxy)
+        gclient.add_dir('/system/proxy', gconf.CLIENT_PRELOAD_NONE)
+        gclient.notify_add('/system/proxy', self.__set_proxy)
             
         self.ui.show_login()
         try:
@@ -92,6 +102,31 @@ class Turpial:
             self.log.debug('Interceptado Keyboard Interrupt')
             self.signout()
         
+    def __set_proxy(self, client, *args, **kwargs):
+        proxies = {}
+        if client.get_bool('/system/http_proxy/use_http_proxy'):
+            proxies['http'] = "http://%s:%d" % (client.get_string('/system/http_proxy/host'), client.get_int('/system/http_proxy/port'))
+            if client.get_bool('/system/http_proxy/use_same_proxy'):
+                proxies['https'] = proxies['http'].replace('http:', 'https:')
+            elif client.get_string('/system/proxy/secure_host'):
+                proxies['https'] = "https://%s:%d" % (gclient.get_string('/system/http/secure_host'), gclient.get_int('/system/proxy/secure_port'))
+
+        if proxies:
+            if client.get_bool('/system/http_proxy/use_authentication'):
+                self.log.debug('Usando proxy con autenticacion')
+                puser = urllib.quote(client.get_string('/system/http_proxy/authentication_user'))
+                ppass = urllib.quote(client.get_string('/system/http_proxy/authentication_password'))
+                for schema, proxy in proxies.items():
+                    proxies[schema] = proxy.replace("%s://" % schema, "%s://%s:%s@" % (schema, puser, ppass))
+            else:
+                self.log.debug('Usando proxy sin autenticacion')
+            proxy_handler = urllib2.ProxyHandler(proxies)
+            opener = urllib2.build_opener(proxy_handler, urllib2.HTTPHandler)
+        else:
+            self.log.debug('No usando proxies')
+            opener = urllib2.build_opener()
+        urllib2.install_opener(opener)
+
     def __clean(self):
         '''Limpieza de ficheros .pyc y .pyo'''
         self.log.debug("Limpiando la casa...")
